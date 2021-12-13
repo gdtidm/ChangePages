80d6de5f5fa7b64e958f58e35d6ecc7c57c9232a
Aqshal Rizqullah <aqshalrizqullah89@gmail.com>
2021-12-13 11:17:25 +0700

    Gendut Idaman - Android Changes Default Page

---
+++ app/src/main/java/apps/gdtidm/customlaunch/First.java
@@ -0,0 +1,31 @@
+package apps.gdtidm.customlaunch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class First extends AppCompatActivity {
+
+    Button btn_move;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        btn_move = findViewById(R.id.btn_move);
+        btn_move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(First.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+}
